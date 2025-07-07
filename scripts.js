5b862756e059f00a70066dc8b49bfb5a92b1f6eb
Milagros Barrionuevo <101229338+milagrospbc@users.noreply.github.com>
2025-07-07 01:38:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Cambiar navbar al hacer scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('scrolled', window.scrollY > 50);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+
+        question.addEventListener('click', () => {
+            // Cierra todas las demás respuestas abiertas
+            faqItems.forEach(otherItem => {
+                if (otherItem !== item && otherItem.classList.contains('active')) {
+                    otherItem.classList.remove('active');
+                }
+            });
+            
+            // Alterna la clase 'active' para el elemento clicado
+            item.classList.toggle('active');
+        });
+    });
+});
